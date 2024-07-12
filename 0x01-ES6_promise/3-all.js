ed6991fc9e82029b2ac89f8af47e64b8248cd7dd
Antonymaged <magedantony1@gmail.com>
2024-07-12 12:34:24 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup(){
-	return Promise
+	return new Promise
 	.all([uploadPhoto(), createUser()])
 	.then((r) => {console.log(`${r[0].body} ${r[1].firstName} ${r[2].lastName}`)})
 	.catch(() => console.log("Signup system offline"));
