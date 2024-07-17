5769b265e6183ea2728f13904a49b20b522d1f41
Antonymaged <magedantony1@gmail.com>
2024-07-17 03:46:00 +0000

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,10 +2,10 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor !== Building) {
-      if (typrof this.evacuationWarningMessage !== 'function') {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
           'Class extending Building must override evacuationWarningMessage',
-	);
+        );
       }
     }
   }
