f6c30e077850bf2ac5151da395edb78c64e3538a
Antonymaged <magedantony1@gmail.com>
2024-07-17 02:14:42 +0000

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (name !== 'string') {
+      throw new Error('name is not a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (length !== 'number') {
+      throw new Error('length is not a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new Error('students is not an array');
+    }
+  }
+}
