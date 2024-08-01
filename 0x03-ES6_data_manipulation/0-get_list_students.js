b2f7dd7f1f10cdd2a4591593ce33620032440740
Antonymaged <magedantony1@gmail.com>
2024-08-01 15:18:01 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,4 @@
+export default function getListStudents() {
+  const x = [{ firstName: 'Guillaume', id: 1, location: 'San Francisco' }, { firstName: 'James', id: 2, location: 'Columbia' }, { firstName: 'Serena', id: 5, location: 'San Francisco' }];
+  return x;
+}
