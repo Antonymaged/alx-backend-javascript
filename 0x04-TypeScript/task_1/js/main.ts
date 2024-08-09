5f044fd3a1bd586a64af76fd59694f974884fca1
Antonymaged <magedantony1@gmail.com>
2024-08-09 07:45:17 -0700

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,21 @@
-interface Teacher{
+interface Teacher {
   readonly firstName: string,
   readonly lastName: string,
   fullTimeEmployee: boolean,
   yearsOfExperience: number,
   location: string,
   [key: string]: unknown,
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacher {
+  (firstName: string, secondName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string):string {
+  const fl = firstName.split("")[0];
+  return `${fl}. ${lastName}`;
 }
