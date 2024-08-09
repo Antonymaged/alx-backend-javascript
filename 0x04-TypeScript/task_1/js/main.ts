45aea20a08e36e482a22da57faff462b3000401d
Antonymaged <magedantony1@gmail.com>
2024-08-09 08:26:16 -0700

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,32 @@ interface printTeacher {
   (firstName: string, secondName: string): string;
 }
 
+class StudentClass {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+interface Studenconstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface Studentclass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
 function printTeacher(firstName: string, lastName: string):string {
   const fl = firstName.split("")[0];
   return `${fl}. ${lastName}`;
